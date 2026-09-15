e9de1f1634ab89ebc90e0b510e0988c64dd6f1e6
Frank Ochieng <304288291+fr-konuko@users.noreply.github.com>
2026-09-15 17:41:04 +0300

    Allow Sanity image CDN

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,18 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   poweredByHeader: false,
+
   images: {
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'images.pexels.com',
         pathname: '/photos/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.sanity.io',
+        pathname: '/images/**',
       },
     ],
   },
